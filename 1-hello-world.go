4423a1584dcad1c71ddbd6de6edb70b0776fa09e
Keshavdulal <Keshav.dulal@gmail.com>
2017-12-18 15:32:45 +0545

    master: add comment

--- 1-hello-world.go
+++ 1-hello-world.go
@@ -1,7 +1,10 @@
+// pretty much like public static void main
 package main
 
+// imports, always use double quotes
 import "fmt"
 
+// goland is colloquial-ish in nature yall know soon
 func main(){
 	fmt.Println("Hello, World this is Go")
 }
